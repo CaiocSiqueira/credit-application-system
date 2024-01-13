d92ae31167048b1653f3d209c049c793824e8a8b
Caio Siqueira <caiocesar.siqueira21@gmail.com>
2024-01-13 00:56:46 -0300

    Add ICreditService interface

--- credit-application-system/src/main/kotlin/com/mrsnow/credit/application/system/service/ICreditService.kt
+++ credit-application-system/src/main/kotlin/com/mrsnow/credit/application/system/service/ICreditService.kt
@@ -1,4 +1,12 @@
 package com.mrsnow.credit.application.system.service
 
+import com.mrsnow.credit.application.system.entity.Credit
+import java.util.UUID
+
 interface ICreditService {
+    fun save(credit: Credit): Credit
+
+    fun findAllByCustomer(customerId: Long): List<Credit>
+
+    fun findByCreditCode(customerId: Long, creditCode: UUID): Credit
 }
